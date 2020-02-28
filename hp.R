561dcdeb842ba0203e28b5358a3c12122f170241
fcoeustaquio <fcoeustaquio@gmail.com>
2020-02-28 18:08:56 -0300

    06: Reorganização de arquivos

--- hp.R
+++ hp.R
@@ -1,7 +1,8 @@
 library(holepunch)
-setwd("./github/rvmsmg/01")
+
 write_compendium_description(package     = "rvmsmg01", 
-                             description = "Avaliação das informações hidrométricas: bacia do rio Doce")
+                             description = "Avaliação das informações hidrométricas: bacia do rio Doce",
+                             path        = "./01")
 # to write a description, with dependencies listed 
 # It's good practice to now go fill in the placeholder text.
 
